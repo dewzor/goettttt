4f0d296ba01f87a9408db8ed07ef4f34f8d78c7e
kevinkarlsson94 <kiwii.94@hotmail.com>
2016-01-26 23:14:09 +0100

    commenti

--- DateSite/DateSite/DateSite/Controllers/BrowseProfilesController.cs
+++ DateSite/DateSite/DateSite/Controllers/BrowseProfilesController.cs
@@ -17,24 +17,39 @@ namespace DateSite.Controllers
             _usersRepository = new UsersRepository();
         }
 
-        // GET: BrowseProfiles
+        /// <summary>
+        /// browsar alla users i databasen som inte är invisible
+        /// </summary>
+        /// <returns></returns>
         public ActionResult Browse()
         {
             BrowseModel browseData = new BrowseModel();
             browseData.profiles = _usersRepository.fetchProfiles();
-            browseData.countries = reader.getCountries();
+            browseData.countries = reader.getCountries(); 
             return View(browseData);
         }
 
+
+        /// <summary>
+        /// söker efter användare beroende på inmatat data
+        /// </summary>
+        /// <param name="searchtext"></param>
+        /// <returns></returns>
         [HttpPost]
         public ActionResult Browse(string searchtext)
         {
             BrowseModel browseData = new BrowseModel();
-            browseData.profiles = _usersRepository.findProfilesByName(searchtext);
+            browseData.profiles = _usersRepository.findProfilesByName(searchtext);  //kollar om användaren finns 
             browseData.countries = reader.getCountries();
             return View(browseData);
         }
 
+
+        /// <summary>
+        /// view en user
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         public ActionResult BrowseUser(int id)
         {
             
@@ -49,6 +64,12 @@ namespace DateSite.Controllers
             return View(profile);
         }
 
+
+        /// <summary>
+        /// kollar på en användare, om man ej är inloggad så redirectas man till registersidan
+        /// </summary>
+        /// <param name="idd"></param>
+        /// <returns></returns>
         public ActionResult BrowseUserRandom(int idd)
         {
             int? inputid = idd;
@@ -59,6 +80,13 @@ namespace DateSite.Controllers
                 return RedirectToAction("Register", "Account");
         }
 
+
+        /// <summary>
+        /// lägger till en vän
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="friendid"></param>
+        /// <returns></returns>
         public ActionResult AddFriend(int id, int friendid)
         {
             _friendRepository.SendFRequest(id, friendid);
